1c888561d179b23609018c1c365b655ba5e0f5e0
jesuscarl0695 <sancheja123@gmail.com>
2020-09-29 17:42:12 +0800

    3

--- sancheja_lists.dart
+++ sancheja_lists.dart
@@ -7,10 +7,14 @@ void main(){
  //print("$haha");
  List<int> numbers = [100,35,66,24,90,33];
  int sum=0;
+ double averages;
   for(var i=0 ; i<= numbers.length-1 ; i++){
        sum += numbers[i];
+       averages = sum / i;
+       
 
   }
   print("Sum: $sum");
+    print("Averages: $averages");
 
 }
