c4b512be9c9519b979a3fdaf91b2b2912fcb281c
jesuscarl0695 <sancheja123@gmail.com>
2020-09-30 23:30:46 +0800

    FINAL OUTPUT

--- main.dart
+++ main.dart
@@ -1,14 +1,26 @@
 import "triangle.dart";
 void main(){
-Triangle triangle1 = new Triangle();
-Triangle triangle2 = new Triangle();
-Triangle triangle3 = new Triangle();
-Triangle triangle4= new Triangle();
+Triangle triangle1 = new Triangle(18,30,24);
+Triangle triangle2 = new Triangle(9,9,9);
+Triangle triangle3 = new Triangle(7,4,7);
+Triangle triangle4= new Triangle(0,1,4);
 
+print("=== Triangle 1 ===");
+triangle1.getSides();
+print("Perimeter: ${triangle1.getPerimeter()}");
+print("Area: ${triangle1.getArea()}");
+print("=== Triangle 2 ===");
+triangle2.getSides();
+print("Perimeter: ${triangle2.getPerimeter()}");
+print("Area: ${triangle2.getArea()}");
+print("=== Triangle 3 ===");
+triangle3.getSides();
+print("Perimeter: ${triangle3.getPerimeter()}");
+print("Area: ${triangle3.getArea()}");
+print("=== Triangle 4 ===");
+triangle4.getSides();
+print("Perimeter: ${triangle4.getPerimeter()}");
+print("Area: ${triangle4.getArea()}");
 
-triangle1.Inputsides(1, 2, 3);
-triangle2.Inputsides(1, 2, 3);
-triangle3.Inputsides(1, 2, 3);
-triangle4.Inputsides(1, 2, 3);
 
 }
--- triangle.dart
+++ triangle.dart
@@ -1,13 +1,40 @@
+import "dart:math";
 class Triangle{
   double _side1,_side2,_side3;
-   
-  Inputsides(double side1,double side2,double side3){
+  Triangle(double side1,double side2,double side3){
     _side1 = side1;
     _side2 = side2;
     _side3 = side3;
-    print("Sides: $_side1,$_side2,$_side3");
-
+   
   }
-
+  void getSides(){
+    print("Sides: $_side1 , $_side2 , $_side3");
+  }
+  
+  bool isTriangle(_side1,_side2,_side3){
+          if ((_side1 + _side2 > _side3) 
+          && (_side2 + _side3 > _side1) 
+          && (_side3 + _side1 > _side2)
+          && (_side1 > 0) && (_side2 >0)
+          &&(_side3 >0) )
+          {
+            return true;
+          }
+            return false;
+  }
+  double getPerimeter(){
+      if(isTriangle(_side1, _side2, _side3)==false){
+        return 0;
+      }
+      return _side1+_side2+_side3;
+  }
+   double getArea(){
+      if(isTriangle(_side1, _side2, _side3)==false){
+        return 0;
+      }
+      var p = (_side1+_side2+_side3)/2;
+      var ans =  p*(p-_side1)*(p-_side2)*(p-_side3);
+      return sqrt(ans);
+  }
 
 }
