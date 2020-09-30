12ed0dc3b49e0b369833c33258f30df60b9affe0
jesuscarl0695 <sancheja123@gmail.com>
2020-09-30 22:13:18 +0800

    progress2

---
+++ main.dart
@@ -0,0 +1,14 @@
+import "triangle.dart";
+void main(){
+Triangle triangle1 = new Triangle();
+Triangle triangle2 = new Triangle();
+Triangle triangle3 = new Triangle();
+Triangle triangle4= new Triangle();
+
+
+triangle1.Inputsides(1, 2, 3);
+triangle2.Inputsides(1, 2, 3);
+triangle3.Inputsides(1, 2, 3);
+triangle4.Inputsides(1, 2, 3);
+
+}
---
+++ triangle.dart
@@ -0,0 +1,13 @@
+class Triangle{
+  double _side1,_side2,_side3;
+   
+  Inputsides(double side1,double side2,double side3){
+    _side1 = side1;
+    _side2 = side2;
+    _side3 = side3;
+    print("Sides: $_side1,$_side2,$_side3");
+
+  }
+
+
+}
